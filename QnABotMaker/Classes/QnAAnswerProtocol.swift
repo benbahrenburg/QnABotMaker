1c4d2dda68ed6acaebd3f3ebab0b1cac3bfbdc6e
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2018-05-16 22:27:29 -0400

    Update copy right

--- QnABotMaker/Classes/QnAAnswerProtocol.swift
+++ QnABotMaker/Classes/QnAAnswerProtocol.swift
@@ -5,7 +5,7 @@
  
  MIT License
  
- Copyright (c) 2017 Ben Bahrenburg @bencoding
+ Copyright (c) 2018 Ben Bahrenburg @bencoding
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
